2f536a56c28c607bdc36706ea211f33e0e71ecdf
Karen Tamer <karen.poot@cbtis72.edu.mx>
2022-03-07 01:27:31 -0500

    Definir eventos

--- app/src/main/java/com/example/toolbar/MainActivity.kt
+++ app/src/main/java/com/example/toolbar/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.toolbar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
@@ -21,5 +23,15 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.menu, menu)
 
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.bFav -> {
+                Toast.makeText(this, "Elemento añadido como favorito", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else ->{return super.onOptionsItemSelected(item)}
+        }
     }
 }
